53dadc801feff9cdf0d0bb51dc0a38b20c86f65b
Joshua Spinak <jspinak@hotmail.com>
2022-02-24 17:41:08 +0100

    Lib: fix TRANSFER attribute output. last push in version 1.0.2.

--- library/src/main/java/io/github/jspinak/brobot/buildStateStructure/buildFromNames/babyStates/BabyState.java
+++ library/src/main/java/io/github/jspinak/brobot/buildStateStructure/buildFromNames/babyStates/BabyState.java
@@ -1,10 +1,13 @@
 package io.github.jspinak.brobot.buildStateStructure.buildFromNames.babyStates;
 
+import io.github.jspinak.brobot.buildStateStructure.buildFromNames.attributes.AttributeTypes;
 import io.github.jspinak.brobot.database.state.stateObject.stateImageObject.StateImageObject;
 import lombok.Getter;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 public class BabyState {
@@ -19,6 +22,12 @@ public class BabyState {
 
     public void addImage(StateImageObject stateImageObject) {
         images.add(stateImageObject);
+    }
+
+    public List<StateImageObject> getImagesByAttributeAndPage(AttributeTypes.Attribute attribute, int page) {
+        return images.stream()
+                .filter(image -> image.getAttributes().getActiveAttributes(page).contains(attribute))
+                .collect(Collectors.toList());
     }
 
 }
