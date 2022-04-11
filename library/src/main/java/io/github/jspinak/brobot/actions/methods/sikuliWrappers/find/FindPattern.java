5ada627b7d049c2ff0501ab80c854f350910b01e
Joshua Spinak <jspinak@hotmail.com>
2022-04-11 07:28:12 +0200

    Lib: Added unit testing capabilities, which allow you to test specific functions on specific screenshots. Find and Highlight operations are performed live on the screenshot, and all other operations are mocked. The screenshot is accessed in file and does not need to be on the screen.

--- library/src/main/java/io/github/jspinak/brobot/actions/methods/sikuliWrappers/find/FindPattern.java
+++ library/src/main/java/io/github/jspinak/brobot/actions/methods/sikuliWrappers/find/FindPattern.java
@@ -1,5 +1,6 @@
 package io.github.jspinak.brobot.actions.methods.sikuliWrappers.find;
 
+import io.github.jspinak.brobot.actions.BrobotSettings;
 import io.github.jspinak.brobot.actions.actionOptions.ActionOptions;
 import io.github.jspinak.brobot.actions.methods.time.Time;
 import io.github.jspinak.brobot.database.primitives.match.MatchObject;
@@ -7,11 +8,14 @@ import io.github.jspinak.brobot.database.primitives.match.Matches;
 import io.github.jspinak.brobot.database.primitives.region.Region;
 import io.github.jspinak.brobot.database.state.stateObject.stateImageObject.StateImageObject;
 import org.sikuli.script.FindFailed;
+import org.sikuli.script.Finder;
 import org.sikuli.script.Match;
 import org.sikuli.script.Pattern;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -31,7 +35,31 @@ public class FindPattern {
     /**
      * The Sikuli function 'find' finds the best Match for a Sikuli Pattern.
      */
-    public Optional<Match> findSikuli(Region region, Pattern pattern) {
+    public Optional<Match> findBest(Region region, Pattern pattern) {
+        if (BrobotSettings.screenshot.isEmpty()) return findLive(region, pattern);
+        return findInScreenshot(region, pattern);
+    }
+
+    private Finder getFinder() {
+        String filename = BrobotSettings.screenshotPath + BrobotSettings.screenshot;
+        File file = new File(filename);
+        String path = file.getAbsolutePath();
+        return new Finder(path);
+    }
+
+    private Optional<Match> findInScreenshot(Region region, Pattern pattern) {
+        Optional<Match> matchOptional = Optional.empty();
+        Finder f = getFinder();
+        f.find(pattern);
+        if (f.hasNext()) {
+            Match nextMatch = f.next();
+            if (region.contains(nextMatch)) matchOptional = Optional.of(nextMatch);
+        }
+        f.destroy();
+        return matchOptional;
+    }
+
+    private Optional<Match> findLive(Region region, Pattern pattern) {
         try {
             Match match = region.sikuli().find(pattern);
             return Optional.of(match);
@@ -41,17 +69,43 @@ public class FindPattern {
 
     public Matches findAll(Region region, Pattern pattern, StateImageObject stateImageObject,
                            ActionOptions actionOptions) {
+        if (BrobotSettings.screenshot.isEmpty())
+            return findAllLive(region, pattern, stateImageObject, actionOptions);
+        return findAllInScreenshot(region, pattern, stateImageObject, actionOptions);
+    }
+
+    private void addMatch(Matches matches, Match match, StateImageObject stateImageObject,
+                     ActionOptions actionOptions) {
+        try {
+            matches.add(new MatchObject(match, stateImageObject,
+                    time.getDuration(actionOptions.getAction()).getSeconds()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Matches findAllInScreenshot(Region region, Pattern pattern, StateImageObject stateImageObject,
+                                       ActionOptions actionOptions) {
+        Finder f = getFinder();
+        f.findAll(pattern);
+        Matches matches = new Matches();
+        while (f.hasNext()) {
+            Match nextMatch = f.next();
+            if (region.contains(nextMatch))
+                addMatch(matches, nextMatch, stateImageObject, actionOptions);
+        }
+        f.destroy();
+        return matches;
+    }
+
+    public Matches findAllLive(Region region, Pattern pattern, StateImageObject stateImageObject,
+                           ActionOptions actionOptions) {
         Matches matches = new Matches();
         Iterator<Match> newMatches;
         try {
             newMatches = region.sikuli().findAll(pattern);
             while (newMatches.hasNext()) {
-                try {
-                    matches.add(new MatchObject(newMatches.next(), stateImageObject,
-                            time.getDuration(actionOptions.getAction()).getSeconds()));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                addMatch(matches, newMatches.next(), stateImageObject, actionOptions);
             }
         } catch (FindFailed ignored) {}
         return matches;
