a2a4e2dc9ea64e277c29bf444ef440dcf5d9231d
Joshua Spinak <jspinak@hotmail.com>
2023-01-11 22:34:54 +0100

    Lib: Preparing for new version 1.0.6.

--- library/src/main/java/io/github/jspinak/brobot/actions/methods/basicactions/captureAndReplay/replay/ReplayAction.java
+++ library/src/main/java/io/github/jspinak/brobot/actions/methods/basicactions/captureAndReplay/replay/ReplayAction.java
@@ -1,4 +1,4 @@
-package io.github.jspinak.brobot.actions.methods.basicactions.capture.replay;
+package io.github.jspinak.brobot.actions.methods.basicactions.captureAndReplay.replay;
 
 import io.github.jspinak.brobot.actions.actionExecution.Action;
 import io.github.jspinak.brobot.actions.actionOptions.ActionOptions;
