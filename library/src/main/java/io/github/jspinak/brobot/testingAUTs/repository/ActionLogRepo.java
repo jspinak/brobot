259cb0df4a183317cf56adf418e8aa4887be6b6a
Joshua Spinak <jspinak@hotmail.com>
2023-10-29 21:08:27 +0100

    Lib: Send action logs to http endpoint

--- library/src/main/java/io/github/jspinak/brobot/testingAUTs/repository/ActionLogRepo.java
+++
@@ -1,9 +0,0 @@
-package io.github.jspinak.brobot.testingAUTs.repository;
-
-import io.github.jspinak.brobot.testingAUTs.model.ActionLog;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-public interface ActionLogRepo extends ElasticsearchRepository<ActionLog, String> {
-}
