c951f4b8b2bd1b88eaffd6e54a101dbc563a8ed6
Joshua Spinak <jspinak@hotmail.com>
2024-04-24 10:49:25 +0200

    Change application context for nonheadless environment.

--- app/src/main/java/io/github/jspinak/brobot/app/BrobotApplication.java
+++ app/src/main/java/io/github/jspinak/brobot/app/BrobotApplication.java
@@ -4,7 +4,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
@@ -25,6 +27,9 @@ public class BrobotApplication {
 	}
 
 	public static void main(String[] args) {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(BrobotApplication.class);
+		builder.headless(false);
+		ConfigurableApplicationContext context = builder.run(args);
 		SpringApplication.run(BrobotApplication.class, args);
 	}
 
