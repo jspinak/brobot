cd019a19aedfb02e49c5f839bcaa1761fbabeef0
Joshua <jspinak@hotmail.com>
2024-09-21 16:52:54 +0200

    Provide a default implementation in the brobot library for logging interfaces.

--- library/src/main/java/io/github/jspinak/brobot/logging/TestSessionLogger.java
+++ library/src/main/java/io/github/jspinak/brobot/logging/TestSessionLogger.java
@@ -4,9 +4,18 @@ package io.github.jspinak.brobot.logging;
  * This interface manages the logging session.
  */
 public interface TestSessionLogger {
-    String startSession(String applicationUnderTest);
-    void endSession(String sessionId);
-    void setCurrentState(String sessionId, String stateName, String stateDescription);
+    default String startSession(String applicationUnderTest) {
+        // No-op implementation, returning null
+        return null;
+    }
+
+    default void endSession(String sessionId) {
+        // No-op implementation
+    }
+
+    default void setCurrentState(String sessionId, String stateName, String stateDescription) {
+        // No-op implementation
+    }
 }
 
 
