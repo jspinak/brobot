7b86a8a609be94547276112eec9f25b11ced5fc0
Joshua <jspinak@hotmail.com>
2025-04-07 20:12:06 +0200

    [11,79,80,81] Create gradle module for the desktop runner with JavaFX dependencies, configure directory structure and packages, add README and update gitignore and settings.gradle

---
+++ brobot-runner/src/test/java/io/github/jspinak/brobot/runner/BrobotRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jspinak.brobot.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrobotRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
