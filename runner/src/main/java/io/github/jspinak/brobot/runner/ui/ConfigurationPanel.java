eb96709d96ab57b203e81681c54b7afcde8528f0
Joshua <jspinak@hotmail.com>
2025-05-19 00:13:49 +0200

    Fix JavaFX application start issues.

--- runner/src/main/java/io/github/jspinak/brobot/runner/ui/ConfigurationPanel.java
+++ runner/src/main/java/io/github/jspinak/brobot/runner/ui/ConfigurationPanel.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import net.rgielen.fxweaver.core.FxmlView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +25,7 @@ import java.util.Optional;
 /**
  * JavaFX component for configuring the Brobot Runner
  */
+@FxmlView("")
 public class ConfigurationPanel extends VBox {
     private static ConfigurationPanel INSTANCE;
 
