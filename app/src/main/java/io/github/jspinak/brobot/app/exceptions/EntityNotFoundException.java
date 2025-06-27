46fc70718480a94cd83230259294b330e744c808
Joshua Spinak <jspinak@hotmail.com>
2025-06-27 18:56:31 +0200

    build: Remove deprecated app and web modules

--- app/src/main/java/io/github/jspinak/brobot/app/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.jspinak.brobot.app.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- app/src/main/java/io/github/jspinak/brobot/app/web/requests/CaptureScreenshotsRequest.java
+++
@@ -1,9 +0,0 @@
-package io.github.jspinak.brobot.app.web.requests;
-
-import lombok.Data;
-
-@Data
-public class CaptureScreenshotsRequest {
-    private int secondsToCapture;
-    private double captureFrequency;
-}
