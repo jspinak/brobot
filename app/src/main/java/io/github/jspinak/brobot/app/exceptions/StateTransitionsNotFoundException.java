39300d15f396e32be02ce844483fd5b0faeacea2
Joshua Spinak <jspinak@hotmail.com>
2025-06-27 18:56:31 +0200

    build: Remove deprecated app and web modules

--- app/src/main/java/io/github/jspinak/brobot/app/exceptions/StateTransitionsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package io.github.jspinak.brobot.app.exceptions;
-
-public class StateTransitionsNotFoundException extends RuntimeException {
-    public StateTransitionsNotFoundException(String message) {
-        super(message);
-    }
-}
