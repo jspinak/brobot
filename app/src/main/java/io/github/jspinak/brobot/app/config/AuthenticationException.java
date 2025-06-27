4302f01ba109d71f00cf6bac8e9d9a27c76650ae
Joshua Spinak <jspinak@hotmail.com>
2025-06-27 18:56:31 +0200

    build: Remove deprecated app and web modules

--- app/src/main/java/io/github/jspinak/brobot/app/config/AuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package io.github.jspinak.brobot.app.config;
-
-// Custom exception for authentication failures
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
