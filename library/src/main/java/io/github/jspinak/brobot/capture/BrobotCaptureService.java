1081f5a99468dbeb301500ef4d5c6ae3dfaa04d0
Joshua Spinak <jspinak@hotmail.com>
2025-09-04 17:27:23 +0200

    Use Jakarta PostConstruct.

--- library/src/main/java/io/github/jspinak/brobot/capture/BrobotCaptureService.java
+++ library/src/main/java/io/github/jspinak/brobot/capture/BrobotCaptureService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
